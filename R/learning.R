3cda89068a2e6f90d310aa0321ef81a7475ac1e1
Anna af Hällström <anna_af_hallstrom@yahoo.co.uk>
2023-01-17 11:15:57 +0100

    Loading data from NHANES library and creating new object (dataset) with selected data

--- R/learning.R
+++ R/learning.R
@@ -1,6 +1,7 @@
 # Here's and example of a merge conflict.
+library(tidyverse)
+library(NHANES)
 
-10
 
 # R basics ----------------------------------------------------------------
 
@@ -21,9 +22,50 @@ summary(airquality)
 # command + shift + p -> style active
 2 + 2
 
-# Packages ----------------------------------------------------------------
-
-library(tidyverse)
-
-
 # This will be used for testing out Git
+
+# Looking at data ---------------------------------------------------------
+
+glimpse(NHANES)
+# glimpse(LIBRARY) gives a glimpse at the data
+# int = integer
+# fct = factor
+# dbl = number with decimals
+# data object is always first: all functions have arguments (settings you give it)
+
+select(NHANES, Age)
+# select without assigning: we're just showing it on the screen (console)
+
+select(NHANES, Age, Weight)
+# showing multiple columns from the dataset separated by a comma (,)
+
+select(NHANES, -Age, -HeadCirc)
+# select and exclude by using a minus (-) befor the column you want to exclude
+
+colnames(NHANES)
+# colnames (LIBRARY/DATASET): checks the headings of columns
+
+select(NHANES, starts_with("BP"))
+# remember citation marks around text!
+# other commands: ends_with ("TEXT"), contains("")
+
+select(NHANES, contains("Day"), -contains("Phys"))
+# can be combained with exclude (-) and multiple (,)
+
+# create new object
+nhanes_small <- select(
+  NHANES,
+  Age,
+  Gender,
+  BMI,
+  Diabetes,
+  PhysActive,
+  BPSysAve,
+  BPDiaAve,
+  Education
+)
+# check that new object contains what you want it to: name of object and command + enter
+nhanes_small
+
+# style code: command + shift + p: style
+
